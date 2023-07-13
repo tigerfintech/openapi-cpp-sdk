740c050d793ac72e3731e5cd059565e9df263f60
Su <sukai@itiger.com>
2023-07-13 20:30:33 +0800

    add api

--- include/tigerapi/trade_client.h
+++ include/tigerapi/trade_client.h
@@ -135,9 +135,12 @@ namespace TIGER_API {
                                   SegmentType seg_type = SegmentType::SEC);
 
         Order get_order(unsigned long long id, bool is_brief=false);
-        Order get_transactions(utility::string_t account = U(""), long order_id = 0, utility::string_t sec_type = U(""),
-                              utility::string_t symbol = U(""), long start_time = -1, time_t end_time = -1,
-                              int limit = 100, utility::string_t expiry = U(""), double strike = 0, utility::string_t right = U(""));
+        value get_transactions(utility::string_t account, long order_id);
+        value get_transactions(utility::string_t account, utility::string_t symbol, utility::string_t sec_type = U(""),
+                               long start_time = -1, time_t end_time = -1,
+                               int limit = 100, utility::string_t expiry = U(""), utility::string_t strike = U(""), utility::string_t right = U(""),
+                               long order_id = 0);
+
         value cancel_order(unsigned long long id);
         value place_order(value &order);
         value place_order(Order &order);
@@ -152,8 +155,19 @@ namespace TIGER_API {
         value
         get_contract(utility::string_t symbol, SecType sec_type = SecType::STK, Currency currency = Currency::ALL, utility::string_t exchange = U(""), time_t expiry = -1,
                      utility::string_t strike = U(""), Right right = Right::ALL);
+        value get_contracts(const value &symbols, utility::string_t sec_type, utility::string_t currency = U(""), utility::string_t exchange = U(""), time_t expiry = -1,
+                            utility::string_t strike = U(""), utility::string_t right = U(""));
+        value get_quote_contract(utility::string_t symbol, utility::string_t sec_type, utility::string_t expiry);
 
+        value get_estimate_tradable_quantity(Order &order, utility::string_t seg_type = U("SEC"));
+        value get_analytics_asset(utility::string_t account, utility::string_t start_date, utility::string_t end_date,
+                                  utility::string_t seg_type = U("SEC"), utility::string_t currency = U("USD"),
+                                  utility::string_t sub_account = U(""));
 
+        value get_segment_fund_history(int limit = 0);
+        value get_segment_fund_available(utility::string_t from_segment, utility::string_t currency = U("USD"));
+        value transfer_segment_fund(utility::string_t from_segment, utility::string_t to_segment, double amount, utility::string_t currency = U("USD"));
+        value place_forex_order(utility::string_t seg_type, utility::string_t source_currency, utility::string_t target_currency, double source_amount);
 
 
     private:
