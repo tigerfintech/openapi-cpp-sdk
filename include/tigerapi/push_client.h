43a88a1fefc6e5c910471d9675dcf003791e479e
chengxin <chengxin@itiger.com>
2024-11-22 21:15:06 +0800

    【ADD】统一将pb头文件放到接口头文件中

--- include/tigerapi/push_client.h
+++ include/tigerapi/push_client.h
@@ -8,18 +8,21 @@
 #include "../include/tigerapi/enums.h"
 #include "../include/openapi_pb/pb_source/Request.pb.h"
 #include "../include/openapi_pb/pb_source/Response.pb.h"
+#include "../include/openapi_pb/pb_source/SocketCommon.pb.h"
+#include "../include/openapi_pb/pb_source/PushData.pb.h"
 #include "../include/openapi_pb/pb_source/AssetData.pb.h"
 #include "../include/openapi_pb/pb_source/PositionData.pb.h"
 #include "../include/openapi_pb/pb_source/OrderStatusData.pb.h"
+#include "../include/openapi_pb/pb_source/OrderTransactionData.pb.h"
 #include "../include/openapi_pb/pb_source/QuoteData.pb.h"
 #include "../include/openapi_pb/pb_source/QuoteBasicData.pb.h"
 #include "../include/openapi_pb/pb_source/QuoteBBOData.pb.h"
 #include "../include/openapi_pb/pb_source/KlineData.pb.h"
 #include "../include/openapi_pb/pb_source/TickData.pb.h"
+#include "../include/openapi_pb/pb_source/TradeTickData.pb.h"
 #include "../include/openapi_pb/pb_source/StockTopData.pb.h"
 #include "../include/openapi_pb/pb_source/OptionTopData.pb.h"
 #include "../include/openapi_pb/pb_source/QuoteDepthData.pb.h"
-#include "../include/openapi_pb/pb_source/Response.pb.h"
 
 namespace TIGER_API
 {
