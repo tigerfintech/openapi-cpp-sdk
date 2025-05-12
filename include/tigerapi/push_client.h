3179120214dda72b359e2a80f7a13ecfbd5dc69c
Su <sukai@itiger.com>
2025-05-12 11:56:21 +0800

    subscribe request return id

--- include/tigerapi/push_client.h
+++ include/tigerapi/push_client.h
@@ -46,54 +46,54 @@ namespace TIGER_API
 		virtual void set_kickout_callback(const std::function<void(const tigeropen::push::pb::Response&)>& cb) = 0;
 		
 		virtual void set_asset_changed_callback(const std::function<void(const tigeropen::push::pb::AssetData&)>& cb) = 0;
-		virtual bool subscribe_asset(const std::string& account) = 0;
-		virtual bool unsubscribe_asset(const std::string& account) = 0;
+		virtual unsigned int subscribe_asset(const std::string& account) = 0;
+		virtual unsigned int unsubscribe_asset(const std::string& account) = 0;
 		
 		virtual void set_position_changed_callback(const std::function<void(const tigeropen::push::pb::PositionData&)>& cb) = 0;
-		virtual bool subscribe_position(const std::string& account) = 0;
-		virtual bool unsubscribe_position(const std::string& account) = 0;
+		virtual unsigned int subscribe_position(const std::string& account) = 0;
+		virtual unsigned int unsubscribe_position(const std::string& account) = 0;
 
 		virtual void set_order_changed_callback(const std::function<void(const tigeropen::push::pb::OrderStatusData&)>& cb) = 0;
-		virtual bool subscribe_order(const std::string& account) = 0;
-		virtual bool unsubscribe_order(const std::string& account) = 0;
+		virtual unsigned int subscribe_order(const std::string& account) = 0;
+		virtual unsigned int unsubscribe_order(const std::string& account) = 0;
 
 		virtual void set_transaction_changed_callback(const std::function<void(const tigeropen::push::pb::OrderTransactionData&)>& cb) = 0;
-		virtual bool subscribe_transaction(const std::string& account) = 0;
-		virtual bool unsubscribe_transaction(const std::string& account) = 0;
+		virtual unsigned int subscribe_transaction(const std::string& account) = 0;
+		virtual unsigned int unsubscribe_transaction(const std::string& account) = 0;
 		
 		virtual void set_query_subscribed_symbols_changed_callback(const std::function<void(const tigeropen::push::pb::Response& query_subscribed_symbols_response)>& cb) = 0;
-		virtual void query_subscribed_symbols() = 0;
+		virtual unsigned int query_subscribed_symbols() = 0;
 		
 		virtual void set_quote_changed_callback(const std::function<void(const tigeropen::push::pb::QuoteBasicData&)>& cb) = 0;
 		virtual void set_quote_bbo_changed_callback(const std::function<void(const tigeropen::push::pb::QuoteBBOData&)>& cb) = 0;
-		virtual bool subscribe_quote(const std::vector<std::string>& symbols) = 0;
-		virtual bool subscribe_future_quote(const std::vector<std::string>& symbols) = 0;
-		virtual bool subscribe_option_quote(const std::vector<std::string>& symbols) = 0;
-		virtual bool unsubscribe_quote(const std::vector<std::string>& symbols) = 0;
+		virtual unsigned int subscribe_quote(const std::vector<std::string>& symbols) = 0;
+		virtual unsigned int subscribe_future_quote(const std::vector<std::string>& symbols) = 0;
+		virtual unsigned int subscribe_option_quote(const std::vector<std::string>& symbols) = 0;
+		virtual unsigned int unsubscribe_quote(const std::vector<std::string>& symbols) = 0;
 
 		virtual void set_quote_depth_changed_callback(const std::function<void(const tigeropen::push::pb::QuoteDepthData&)>& cb) = 0;
-		virtual bool subscribe_quote_depth(const std::vector<std::string>& symbols) = 0;
-		virtual bool unsubscribe_quote_depth(const std::vector<std::string>& symbols) = 0;
+		virtual unsigned int subscribe_quote_depth(const std::vector<std::string>& symbols) = 0;
+		virtual unsigned int unsubscribe_quote_depth(const std::vector<std::string>& symbols) = 0;
 
 		virtual void set_kline_changed_callback(const std::function<void(const tigeropen::push::pb::KlineData&)>& cb) = 0;
-		virtual bool subscribe_kline(const std::vector<std::string>& symbols) = 0;
-		virtual bool unsubscribe_kline(const std::vector<std::string>& symbols) = 0;
+		virtual unsigned int subscribe_kline(const std::vector<std::string>& symbols) = 0;
+		virtual unsigned int unsubscribe_kline(const std::vector<std::string>& symbols) = 0;
 
 		virtual void set_full_tick_changed_callback(const std::function<void(const tigeropen::push::pb::TickData&)>& cb) = 0;
 		virtual void set_tick_changed_callback(const std::function<void(const TradeTick&)>& cb) = 0;
-		virtual bool subscribe_tick(const std::vector<std::string>& symbols) = 0;
-		virtual bool unsubscribe_tick(const std::vector<std::string>& symbols) = 0;
+		virtual unsigned int subscribe_tick(const std::vector<std::string>& symbols) = 0;
+		virtual unsigned int unsubscribe_tick(const std::vector<std::string>& symbols) = 0;
 
-		virtual bool subscribe_market(const std::string& market) = 0;
-		virtual bool unsubscribe_market(const std::string& market) = 0;
+		virtual unsigned int subscribe_market(const std::string& market) = 0;
+		virtual unsigned int unsubscribe_market(const std::string& market) = 0;
 
 		virtual void set_stock_top_changed_callback(const std::function<void(const tigeropen::push::pb::StockTopData&)>& cb) = 0;
-		virtual bool subscribe_stock_top(const std::string& market) = 0;
-		virtual bool unsubscribe_stock_top(const std::string& market) = 0;
+		virtual unsigned int subscribe_stock_top(const std::string& market) = 0;
+		virtual unsigned int unsubscribe_stock_top(const std::string& market) = 0;
 
 		virtual void set_option_top_changed_callback(const std::function<void(const tigeropen::push::pb::OptionTopData&)>& cb) = 0;
-		virtual bool subscribe_option_top(const std::string& market) = 0;
-		virtual bool unsubscribe_option_top(const std::string& market) = 0;
+		virtual unsigned int subscribe_option_top(const std::string& market) = 0;
+		virtual unsigned int unsubscribe_option_top(const std::string& market) = 0;
 
 	};
 }
