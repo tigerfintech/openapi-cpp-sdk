e2aa900d81366a77c670e9ff943a2bd70af993c4
Su <sukai@itiger.com>
2026-03-04 18:47:33 +0800

    update mac lib

--- demo/openapi_cpp_test/openapi_cpp_test.cpp
+++ demo/openapi_cpp_test/openapi_cpp_test.cpp
@@ -615,14 +615,14 @@ int main(int argc, char* argv[]) {
     //config.lang = U("en_US");
 
     //Create a push client instance
-    //auto push_client = IPushClient::create_push_client(config);
-    //TestPushClient::test_push_client(push_client, config);
+    // auto push_client = IPushClient::create_push_client(config);
+    // TestPushClient::test_push_client(push_client, config);
 
     /**
      *  QuoteClient
      */
-     std::shared_ptr<QuoteClient> quote_client = std::make_shared<QuoteClient>(config);
-     TestQuoteClient::test_quote(quote_client);
+    std::shared_ptr<QuoteClient> quote_client = std::make_shared<QuoteClient>(config);
+    TestQuoteClient::test_quote(quote_client);
 
     /**
      * TradeClient
